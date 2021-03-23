5ad4d9003f4a238642eaf03a1a34d083ae3f5548
Nicola Galeotti <niccio2802@outlook.it>
2021-03-23 12:37:50 +0100

    finite interfacce grafiche

--- Inserimento Personale/Aziendalexaml.xaml.cs
+++ Inserimento Personale/Aziendalexaml.xaml.cs
@@ -10,29 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Inserimento_Personale
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Aziendalexaml.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Aziendalexaml : Window
     {
-        public MainWindow()
+        public Aziendalexaml()
         {
             InitializeComponent();
-        }
-
-        string[] tipologia = new string[] {"Aziendale", "SubFornitore", "Fornitore", "Consulente"};
-
-        private void cmbTipologia_Loaded(object sender, RoutedEventArgs e)
-        {
-            foreach(string s in tipologia)
-            {
-                cmbTipologia.Items.Add(s);
-            }
         }
     }
 }
